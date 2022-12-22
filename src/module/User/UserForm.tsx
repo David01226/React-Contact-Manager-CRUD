f1f2cb4f497d2b7c46b608f5d79ea3ccf1c5b785
David Gibson <90648825+David01226@users.noreply.github.com>
2022-12-22 08:23:32 +0000

    Created User form and functions submit and view

--- src/module/User/UserForm.tsx
+++ src/module/User/UserForm.tsx
@@ -1,5 +1,54 @@
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { RootState } from '../../app/store';
+import { Input } from '../../components/input';
+import { ApiStatus, IUserForm } from './User.type';
+import Style from './UserFormStyle.module.css';
+import { createUserAction, resetCreateListStatus } from './UserSlice';
+
 const UserForm = () => {
-    return <div>This is user form</div>;
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
+    const { createUserFormStatus } = useAppSelector((state: RootState) => state.user)
+    const dispatch = useAppDispatch();
+
+    const onSubmitForm = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const data : IUserForm = { name, email};
+        dispatch(createUserAction(data));
+    };
+
+    useEffect(() => {
+        if (createUserFormStatus === ApiStatus.success) {
+            setName("")
+            setEmail("")
+            dispatch(resetCreateListStatus());
+        }
+    }, [createUserFormStatus])
+
+    return (
+        <div className={Style.container}>
+            <form className={Style.form} onSubmit={onSubmitForm}>
+                <Input label='Name' value={name} onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    setName(e.target.value)
+                    console.log(name)
+                }} />
+
+                <Input label='Email' value={email} type='email' onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    setEmail(e.target.value)
+                }} />
+
+                <div className={Style["btn-wrapper"]}>
+                    <input type="submit" value="Create" />
+                </div>
+
+            </form>
+        </div>
+    )
+    
 };
 
 export default UserForm;
--- src/module/User/UserSlice.ts
+++ src/module/User/UserSlice.ts
@@ -1,25 +1,37 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { ApiStatus, IUserState } from "./User.type";
-import { getUserList } from "./UserService";
+import { ApiStatus, IUserForm, IUserState } from "./User.type";
+import { createUserApi, getUserListApi } from "./UserService";
 
 const initialState: IUserState = {
     list : [],
     listStatus: ApiStatus.ideal,
+    createUserFormStatus: ApiStatus.ideal
 };
 
 export const getUserListAction = createAsyncThunk(
     "user/getUserListAction", 
     async () => {
-        const response = await getUserList()
+        const response = await getUserListApi()
         return response.data;
     }
 );
+
+export const createUserAction = createAsyncThunk(
+    "user/createUserAction",
+    async (data: IUserForm) => {
+        const response = await createUserApi(data)
+        return response.data;
+})
 
 
 const userSlice = createSlice({
     name: "user",
     initialState,
-    reducers: {},
+    reducers: {
+        resetCreateListStatus : (state) => {
+            state.createUserFormStatus = ApiStatus.ideal
+        }
+    },
     extraReducers: (builder) => {
         builder.addCase(getUserListAction.pending, (state) => {
             state.listStatus = ApiStatus.loading
@@ -32,7 +44,18 @@ const userSlice = createSlice({
             state.listStatus = ApiStatus.error;
         });
 
+        builder.addCase(createUserAction.pending, (state) => {
+            state.createUserFormStatus = ApiStatus.loading
+        });
+        builder.addCase(createUserAction.fulfilled, (state) => {
+            state.createUserFormStatus = ApiStatus.success;
+        });
+        builder.addCase(createUserAction.rejected, (state) => {
+            state.createUserFormStatus = ApiStatus.error;
+        });
+
     }
 });
 
 export default userSlice.reducer;
+export const { resetCreateListStatus } = userSlice.actions;
